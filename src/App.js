87a43c90dc2d2e2c4f737d32feb84ec2ef863325
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-02-12 19:38:31 -0800

    finished

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { Route } from 'react-router-dom'
+const App = () => {
+return (
+<div>
+	<Route path="/" exact component={Home} />
+	<Route path="/contact" exact component={Contact} />
+	<Route path="/about" exact component={About} />
 
-const App = () => (
-  <div>
-    <Navigation />
-  </div>
-);
-
-export default App;
+	<Navigation />
+</div>
+	);
+	}
+	export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import Home from "./components/Home";
+// import About from "./components/About";
+// import Contact from "./components/Contact";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
